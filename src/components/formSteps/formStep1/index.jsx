a81e4ef378717af8228eb7276eccce296c80f1fc
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-06-05 14:54:42 +0200

    formik step2

--- src/components/formSteps/formStep1/index.jsx
+++ src/components/formSteps/formStep1/index.jsx
@@ -2,27 +2,63 @@ import 'style.css';
 import React from 'react';
 import { Input } from '../../Inputs';
 import { Button } from '../../button';
+import { Formik } from 'formik';
 
 export const FormStep1 = () => {
   return (
-    <div className="formStep1">
-      <h1> Tvá veselka </h1>
-      <Input
-        className=""
-        type="text"
-        name="nickname"
-        placeholder="nickname"
-        value=""
-      />
+    <Formik
+      initialValues={{
+        nickname: '',
+        password: '',
+      }}
+      // validate={(values) => {
+      //   const errors = {};
+      //   if (!values.email) {
+      //     errors.email = 'Required';
+      //   } else if (
+      //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      //   ) {
+      //     errors.email = 'Invalid email address';
+      //   }
+      //   return errors;
+      // }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <div className="formStep1">
+          <h1> Tvá veselka </h1>
+          <Input
+            className=""
+            type="text"
+            name="nickname"
+            placeholder="nickname"
+            value=""
+          />
 
-      <Input
-        className=""
-        type="password"
-        name="password"
-        placeholder="heslo"
-        value=""
-      />
-      <Button className="" text="Edituj" onClick={() => {}} />
-    </div>
+          <Input
+            className=""
+            type="password"
+            name="password"
+            placeholder="heslo"
+            value=""
+          />
+          <Button className="" text="Edituj" onClick={() => {}} />
+        </div>
+      )}
+    </Formik>
   );
 };
--- src/components/formSteps/formStep2/index.jsx
+++ src/components/formSteps/formStep2/index.jsx
@@ -4,32 +4,75 @@ import { Input } from '../Inputs';
 
 export const FormStep2 = () => {
   return (
-    <div className="formStep2">
-      <div className="weddingPlace">
-        <h1>Sem přidej detaily o Vaší svatbě</h1>
-        <p> Bla bla bla bla bla</p>
-        <Input className="" type="text" name="date" label="dátum" value="" />
-        <Input
-          className=""
-          type="text"
-          name="weddingPlace"
-          label="miesto svadby"
-          value=""
-        />
-        <Input
-          className=""
-          type="text"
-          name="celebrationPlace"
-          label="miesto oslavy"
-          value=""
-        />
-      </div>
+    <Formik
+      initialValues={{
+        weddingPlace: '',
+        celebrationPlace: '',
+        time: '',
+        program: '',
+      }}
+      // validate={(values) => {
+      //   const errors = {};
+      //   if (!values.email) {
+      //     errors.email = 'Required';
+      //   } else if (
+      //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      //   ) {
+      //     errors.email = 'Invalid email address';
+      //   }
+      //   return errors;
+      // }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <div className="formStep2">
+          <div className="weddingPlace">
+            <h1>Sem přidej detaily o Vaší svatbě</h1>
+            <p> Bla bla bla bla bla</p>
+            <Input
+              className=""
+              type="text"
+              name="date"
+              label="dátum"
+              value=""
+            />
+            <Input
+              className=""
+              type="text"
+              name="weddingPlace"
+              label="miesto svadby"
+              value=""
+            />
+            <Input
+              className=""
+              type="text"
+              name="celebrationPlace"
+              label="miesto oslavy"
+              value=""
+            />
+          </div>
 
-      <div className="weddingProgram">
-        <h2>Popište program Vaší svatby</h2>
-        <Input className="" type="text" name="time" value="" />
-        <Input className="" type="text" name="program" value="" />
-      </div>
-    </div>
+          <div className="weddingProgram">
+            <h2>Popište program Vaší svatby</h2>
+            <Input className="" type="text" name="time" value="" />
+            <Input className="" type="text" name="program" value="" />
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 };
