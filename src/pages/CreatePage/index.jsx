67386e5ff93694e9a983c66faed4f2013766b904
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-06-05 18:15:40 +0200

    routa

--- src/pages/CreatePage/index.jsx
+++ src/pages/CreatePage/index.jsx
@@ -4,8 +4,12 @@ import { Formik } from 'formik';
 import { FormStep1 } from '../../components/formSteps/formStep1';
 import { FormStep2 } from '../../components/formSteps/formStep2';
 import { FormStep3 } from '../../components/formSteps/formStep3';
+import { db } from '../../db';
+import { useHistory } from 'react-router-dom';
 
 export const CreatePage = (props) => {
+  let history = useHistory();
+
   const [step, setStep] = useState(0);
   const formikPages = [<FormStep1 />, <FormStep2 />, <FormStep3 />];
 
@@ -40,39 +44,10 @@ export const CreatePage = (props) => {
       //   }
       //   return errors;
       // }}
-      onSubmit={async ({
-        nickname,
-        password,
-        weddingPlace,
-        celebrationPlace,
-        time,
-        program,
-        starter,
-        main,
-        main2,
-        desert,
-        dinner,
-        drinksNonAlco,
-        drinksAlco,
-        others,
-      }) => {
+      onSubmit={async (values) => {
         if (isLastStep()) {
-          await props.onSubmit({
-            nickname,
-            password,
-            weddingPlace,
-            celebrationPlace,
-            time,
-            program,
-            starter,
-            main,
-            main2,
-            desert,
-            dinner,
-            drinksNonAlco,
-            drinksAlco,
-            others,
-          });
+          const res = await db.collection('veselka').add(values);
+          history.push('/preview/:id');
         } else {
           setStep((s) => s + 1);
         }
