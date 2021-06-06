2563ef44698df6b45b458b4b3b6544b7dc2477cc
Daniela Máčalová <Dan.Macalova@gmail.com>
2021-06-06 10:36:34 +0200

    UseHistory

--- src/components/LPHeader/index.jsx
+++ src/components/LPHeader/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '../button';
 import './style.css';
+import { useHistory } from 'react-router-dom';
 
 export const LPHeader = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/create`);
+  };
+
   return (
     <header className="header">
       <h1>VESELKA</h1>
-      <button>Začít</button>
+      <Button text="Začít" onClick={handleClick} />
       <Link to="/edit">Edituj</Link>
       <Link to="/">Domů</Link>
     </header>
--- src/components/LPIntroduction/index.jsx
+++ src/components/LPIntroduction/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { Button } from '../button';
+
 import './style.css';
 
 export const LPIntroduction = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/create`);
+  };
+
   return (
     <div className="introdution">
       <h2>Vytvořte si web pro svou svatbu...blablabla</h2>
-      <button>Začít plánovat</button>
+      <Button text="Začít plánovat" onClick={handleClick} />
     </div>
   );
 };
