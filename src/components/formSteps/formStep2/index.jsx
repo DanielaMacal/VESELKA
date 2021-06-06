4a5b55b8e61bd4e6ac61fd53394a766dad577588
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-06-06 11:36:09 +0200

    formik upravy, vyhytanie chyb

--- src/components/formSteps/formStep2/index.jsx
+++ src/components/formSteps/formStep2/index.jsx
@@ -2,88 +2,65 @@ import React from 'react';
 import { Input } from '../../Inputs';
 import { Formik } from 'formik';
 
-export const FormStep2 = (props) => {
+export const FormStep2 = ({ values, handleChange }) => {
   return (
-    <Formik
-      initialValues={{
-        weddingPlace: '',
-        celebrationPlace: '',
-        time: '',
-        program: '',
-      }}
-      // validate={(values) => {
-      //   const errors = {};
-      //   if (!values.email) {
-      //     errors.email = 'Required';
-      //   } else if (
-      //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      //   ) {
-      //     errors.email = 'Invalid email address';
-      //   }
-      //   return errors;
-      // }}
-      onSubmit={props.onSubmit}
-    >
-      {({ values, handleChange }) => (
-        <div className="formStep2">
-          <div className="weddingPlace">
-            <h1>Sem přidej detaily o Vaší svatbě</h1>
-            <p> Bla bla bla bla bla</p>
-            <Input
-              className=""
-              type="text"
-              name="date"
-              label="dátum"
-              value={values.date}
-              onChange={handleChange('date')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="weddingPlace"
-              label="miesto svadby"
-              value={values.weddingPlace}
-              onChange={handleChange('weddingPlace')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="celebrationPlace"
-              label="miesto oslavy"
-              value={values.celebrationPlace}
-              onChange={handleChange('celebrationPlace')}
-            />
-          </div>
+    <div className="formStep2">
+      <div className="weddingPlace">
+        <h1>Sem přidej detaily o Vaší svatbě</h1>
+        <p> Bla bla bla bla bla</p>
+        <Input
+          className=""
+          type="date"
+          name="date"
+          label="dátum"
+          value={values.date}
+          onChange={handleChange('date')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="weddingPlace"
+          label="miesto svadby"
+          value={values.weddingPlace}
+          onChange={handleChange('weddingPlace')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="celebrationPlace"
+          label="miesto oslavy"
+          value={values.celebrationPlace}
+          onChange={handleChange('celebrationPlace')}
+        />
+      </div>
 
-          <div className="weddingProgram">
-            <h2>Popište program Vaší svatby</h2>
-            <Input
-              className=""
-              type="time"
-              name="timeFrom"
-              label="čas od"
-              value={values.timeFrom}
-              onChange={handleChange('timeFrom')}
-            />
-            <Input
-              className=""
-              type="time"
-              name="timeTo"
-              label="čas do"
-              value={values.timeTo}
-              onChange={handleChange('timeTo')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="program"
-              label="čo sa bude diať"
-              value={values.program}
-              onChange={handleChange('program')}
-            />
-          </div>
-        </div>
-      )}
-    </Formik>
+      <div className="weddingProgram">
+        <h2>Popište program Vaší svatby</h2>
+        <Input
+          className=""
+          type="time"
+          name="timeFrom"
+          label="čas od"
+          value={values.timeFrom}
+          onChange={handleChange('timeFrom')}
+        />
+        <Input
+          className=""
+          type="time"
+          name="timeTo"
+          label="čas do"
+          value={values.timeTo}
+          onChange={handleChange('timeTo')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="program"
+          label="čo sa bude diať"
+          value={values.program}
+          onChange={handleChange('program')}
+        />
+      </div>
+    </div>
   );
 };
--- src/components/formSteps/formStep3/index.jsx
+++ src/components/formSteps/formStep3/index.jsx
@@ -2,107 +2,80 @@ import React from 'react';
 import { Input } from '../../Inputs';
 import { Formik } from 'formik';
 
-export const FormStep3 = (props) => {
+export const FormStep3 = ({ values, handleChange }) => {
   return (
-    <Formik
-      initialValues={{
-        starter: '',
-        main: '',
-        main2: '',
-        desert: '',
-        dinner: '',
-        drinksNonAlco: '',
-        drinksAlco: '',
-        others: '',
-      }}
-      // validate={(values) => {
-      //   const errors = {};
-      //   if (!values.email) {
-      //     errors.email = 'Required';
-      //   } else if (
-      //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      //   ) {
-      //     errors.email = 'Invalid email address';
-      //   }
-      //   return errors;
-      // }}
-      onSubmit={props.onSubmit}
-    >
-      {({ values, handleChange }) => (
-        <div className="formStep3">
-          <div className="weddingFood">
-            <h1>Sem přidej detaily o Vaší svatbě</h1>
-            <h2>Sem přidej Vaše svatební menu</h2>
-            <p> Bla bla bla bla bla</p>
-            <Input
-              className=""
-              type="text"
-              name="starter"
-              label="predkrm"
-              value={values.starter}
-              onChange={handleChange('starter')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="main"
-              label="hlavný chod"
-              value={values.main}
-              onChange={handleChange('main')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="main2"
-              label="hlavný chod 2"
-              value={values.main2}
-              onChange={handleChange('main2')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="desert"
-              label="dezert"
-              value={values.desert}
-              onChange={handleChange('desert')}
-            />
+    <div className="formStep3">
+      <div className="weddingFood">
+        <h1>Sem přidej detaily o Vaší svatbě</h1>
+        <h2>Sem přidej Vaše svatební menu</h2>
+        <p> Bla bla bla bla bla</p>
+        <Input
+          className=""
+          type="text"
+          name="starter"
+          label="predkrm"
+          value={values.starter}
+          onChange={handleChange('starter')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="main"
+          label="hlavný chod"
+          value={values.main}
+          onChange={handleChange('main')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="main2"
+          label="hlavný chod 2"
+          value={values.main2}
+          onChange={handleChange('main2')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="desert"
+          label="dezert"
+          value={values.desert}
+          onChange={handleChange('desert')}
+        />
 
-            <Input
-              className=""
-              type="text"
-              name="dinner"
-              label="večera"
-              value={values.dinner}
-              onChange={handleChange('dinner')}
-            />
+        <Input
+          className=""
+          type="text"
+          name="dinner"
+          label="večera"
+          value={values.dinner}
+          onChange={handleChange('dinner')}
+        />
 
-            <Input
-              className=""
-              type="text"
-              name="drinksNonAlco"
-              label="nealkoholické nápoje"
-              value={values.drinksNonAlco}
-              onChange={handleChange('drinksNonAlco')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="drinksAlco"
-              label="nápoje alko"
-              value={values.drinksAlco}
-              onChange={handleChange('drinksAlco')}
-            />
-            <Input
-              className=""
-              type="text"
-              name="others"
-              label="iné"
-              value={values.others}
-              onChange={handleChange('others')}
-            />
-          </div>
-        </div>
-      )}
-    </Formik>
+        <Input
+          className=""
+          type="text"
+          name="drinksNonAlco"
+          label="nealkoholické nápoje"
+          value={values.drinksNonAlco}
+          onChange={handleChange('drinksNonAlco')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="drinksAlco"
+          label="nápoje alko"
+          value={values.drinksAlco}
+          onChange={handleChange('drinksAlco')}
+        />
+        <Input
+          className=""
+          type="text"
+          name="others"
+          label="iné"
+          value={values.others}
+          onChange={handleChange('others')}
+        />
+      </div>
+    </div>
   );
 };
